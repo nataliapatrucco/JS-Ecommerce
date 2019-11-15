fbe2375f77c9694364c807f67b32b37676a48971
gon <gonbatalb@gmail.com>
2019-11-15 10:43:49 -0300

    merge

--- back/server.js
+++ back/server.js
@@ -42,7 +42,7 @@ app.use("/api", routes);
 app.use("/*", function(req, res, next) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
+// aaa
 db.sync({ force: false })
   .then(() => {
     console.log(chalk.bgGreen(chalk.black("Connected to database." + "")));
